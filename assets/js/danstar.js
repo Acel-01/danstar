4af71289dc41b86f4f42bf948d08905efd585d96
Acel-01 <emekaaladimma@gmail.com>
2020-09-15 17:32:01 +0100

    bug fix

--- assets/js/danstar.js
+++ assets/js/danstar.js
@@ -3,7 +3,7 @@ AOS.init({
 })
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2021 15:37:25").getTime();
+var countDownDate = new Date("Jan 1, 2021 09:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
